1244005d9d98c9dbb1c9c8c19f97e95f85d06d34
bcot-code <78989054+bcot-code@users.noreply.github.com>
2023-12-15 18:56:07 -0600

    setting the database

---
+++ config/connection.js
@@ -0,0 +1,18 @@
+require("dotenv").config();
+
+const Sequelize = require("sequelize");
+// create a connection object
+const sequelize = process.env.JAWSDB_URL
+  ? new Sequelize(process.env.JAWSDB_URL)
+  : //database name , username, password
+    new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+      //database location
+      host: "localhost",
+      //what sql server is it is:
+      dialect: "mysql",
+      dialectOptions: {
+        decimalNumbers: true,
+      },
+    });
+
+module.exports = sequelize;
