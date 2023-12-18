5e21da619ad2fa21cf984802ebbd33c66dc9df8f
bcot-code <78989054+bcot-code@users.noreply.github.com>
2023-12-17 19:58:52 -0600

    no changes

--- models/index.js
+++ models/index.js
@@ -18,12 +18,13 @@ Category.hasMany(Product, {
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   through: ProductTag,
-  as: "tagged_products",
+  foreignKey: "product_id",
 });
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
   through: ProductTag,
+  foreignKey: "tag_id",
 });
 
 module.exports = {
